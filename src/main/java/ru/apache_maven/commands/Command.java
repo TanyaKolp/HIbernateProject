76935e09254ddadb82223196600861c8a76ebc46
TanyaKolp <lancerchg@gmail.com>
2017-06-15 10:46:05 +0300

    New structure of app2

--- src/main/java/ru/apache_maven/commands/Command.java
+++ src/main/java/ru/apache_maven/commands/Command.java
@@ -8,7 +8,6 @@ import java.util.List;
  * Created by tania on 12/2/16.
  */
 public interface Command {
-    public void execute();
     public Result execute(List<String> input);
     public String getHelp();
 }
